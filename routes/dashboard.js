80b115c66b1a6f66ae5e38097d875ce9eebc77d8
ahmedkrmn <20889958+ahmedkrmn@users.noreply.github.com>
2019-05-04 02:31:31 +0200

    Require login to access the dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const mongoose = require('mongoose');
-
+const requireLogin = require('../middlewares/requireLogin');
 const User = mongoose.model('users');
 const Survey = mongoose.model('surveys');
 
-router.get('/dashboard', async (req, res) => {
+router.get('/dashboard', requireLogin, async (req, res) => {
   const existingUser = await User.findById(req.user.id);
   const userSurveys = await Survey.find({ _user: req.user.id })
     .select({
